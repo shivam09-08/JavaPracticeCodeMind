a5022220fdcd1e0d9cc25a2aabcf056d3e6f9990
shivam09-08 <sailor.shivam083@gmail.com>
2024-02-03 15:34:52 +0530

    Frequency of number
    To Count the frequency of numbers given in an array.

--- src/CountFrequencyOfEachWord.java
+++ src/CountFrequencyOfEachWord.java
@@ -10,26 +10,40 @@ public class CountFrequencyOfEachWord {
 	
 	public void countFrequency(int arr1[])
 	{
-		int count =0;
+		
 		
 		
 		for(int i=0;i<arr1.length;i++)
 		{
+			int flag =0;
+			int count =0;
 			
-		for(int j=1;j<i;j++)	
+		for(int j=i+1;j<arr1.length;j++)	
 		{		
 			if(arr1[i]==arr1[j])
 			{
-				int val = arr1[i];
-				count= count + arr1[i];
+				flag =1;
+//				int val = arr1[i];
+//				count= count + arr1[i];
 				
+				break;
+//				System.out.println("Repeating values are :" + val + " " + " there count are =" + count);
 				
-				System.out.println("Repeating values are :" + val + " " + " there count are =" + count);
-				count ++;
 			}
 			
 			
 		}
+		if(flag==1)
+		continue;
+		for(int j=0;j<=i;j++)
+		{
+			
+			if(arr1[i]==arr1[j])
+			{
+				count++;
+			}
+		}
+		System.out.println("Repeating values are :" + arr1[i] + " " + " there count are =" + count);
 		
 		}
 	}
