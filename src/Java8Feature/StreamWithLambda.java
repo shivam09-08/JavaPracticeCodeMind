4faae4ca78cbc9195f2167c9d062eab358d0f8c9
shivam09-08 <sailor.shivam083@gmail.com>
2024-03-27 13:28:07 +0530

    Using map Reduce Filter functions

--- src/Java8Feature/StreamWithLambda.java
+++ src/Java8Feature/StreamWithLambda.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamWithLambda {
@@ -17,8 +18,17 @@ public class StreamWithLambda {
 	
 	List<String> val = Arrays.asList("Shiv","Abhi","Sumit","Neetu","Vikas");
 	Set<String> names = new HashSet<String>();
-			names.addAll(val);
+			
+	names.addAll(val);
 	System.out.println(names);
+	
+	Integer sum =number.stream().mapToInt(Integer::intValue).sum();
+	System.out.println("sum of list elements are : "+sum);
+	
+	Integer reduces = number.stream().reduce(0, (a,b)-> a-b);
+	List <Integer> map = number.stream().map(e->e*2).collect(Collectors.toList());
+	System.out.println("Multiplication of a list elements are : " + map);
+	System.out.println("substraction of a list elements are : " +reduces);
 	
 	names.stream().filter(e->e.startsWith("S")).forEach(e-> System.out.println(e));
 //	
@@ -33,8 +43,8 @@ public class StreamWithLambda {
 	System.out.println("Total numbers of elements in a list is : " +counts);
 	number.stream().distinct().sorted().forEach(e-> System.out.println("Sorted values are as follow : "+e));
 	
-	
-	
+	List <String > filtername =names.stream().filter(e->e.endsWith("s")).collect(Collectors.toList());
+	filtername.stream().forEach(e->System.out.println("name ends with s are : " + e));
 	
 	}
 
